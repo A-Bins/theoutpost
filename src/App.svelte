084d04f90fafb84f702b38760bd8e2f2488cd333
A-Bins <yyangsuk33@naver.com>
2023-02-02 23:04:06 +0900

    add to temp_icon view.

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,46 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+		<div class="menubar">
+			<img src="/copied_icon_40px.png" class="logo" alt="logo" />
+		<!-- <a href="https://vitejs.dev" target="_blank" rel="noreferrer">  -->
+					
+		<!-- </a> -->
+		</div>
+	<h1 >The Outpost</h1>
 
-  <div class="card">
-    <Counter />
-  </div>
+	<!-- <div class="card"> -->
+		<!-- <Counter /> -->
+	<!-- </div> -->
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+	.menubar { 
+		position: relative;
+  	top: 0;
+  	left: 0;
+  	right: 0;
+		height: 60px;
+		width: 100%;
+		display: flex;
+		color: aqua;
+		align-items: center;
+		justify-content: space-around;
+	}
+	.logo {
+		height: 50px;
+		position: absolute;
+		/* 최상단으로부터 20px 떨어져있음 */
+		/* bottom: 300px; */
+
+		will-change: filter;
+	}
+	.logo:hover {
+		filter: drop-shadow(0 0 2em #646cffaa);
+	}
+	/* .click-to-discord {
+		color: #888;
+	} */
 </style>
